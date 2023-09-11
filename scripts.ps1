8263eda832f812d0906721f5486c2d8960391ac7
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-09-11 11:05:47 +0530

    Create scripts.ps1

---
+++ scripts.ps1
@@ -0,0 +1,63 @@
+# Define the key length (16 bytes for AES-128, 24 bytes for AES-192, 32 bytes for AES-256)
+$keyLength = 32
+
+# Generate a random encryption key
+$keyBytes = [byte[]]::new($keyLength)
+$randomNumberGenerator = [System.Security.Cryptography.RandomNumberGenerator]::Create()
+$randomNumberGenerator.GetBytes($keyBytes)
+
+# Convert the key to a hexadecimal string
+$keyHex = [System.BitConverter]::ToString($keyBytes) -replace '-'
+
+# Display the generated key
+Write-Host "Generated Key (Hex): $keyHex"
+
+# Define your plaintext data and key
+$plaintext = "Hello, World!"
+$key = $keyHex  # 16, 24, or 32 bytes key for AES-128, AES-192, or AES-256
+
+# Convert the key to bytes (UTF-8 encoding)
+$keyBytes = [System.Text.Encoding]::UTF8.GetBytes($key)
+
+# Create a new AES object with the specified key and AES mode
+$AES = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+$AES.Key = $keyBytes
+$AES.Mode = [System.Security.Cryptography.CipherMode]::CBC
+
+# Generate a random initialization vector (IV)
+$AES.GenerateIV()
+$IV = $AES.IV
+
+# Convert plaintext to bytes (UTF-8 encoding)
+$plaintextBytes = [System.Text.Encoding]::UTF8.GetBytes($plaintext)
+
+# Encrypt the data
+$encryptor = $AES.CreateEncryptor()
+$encryptedBytes = $encryptor.TransformFinalBlock($plaintextBytes, 0, $plaintextBytes.Length)
+
+# Convert the IV and encrypted bytes to Base64 strings for storage/transmission
+$IVBase64 = [System.Convert]::ToBase64String($IV)
+$encryptedBase64 = [System.Convert]::ToBase64String($encryptedBytes)
+
+# Display the encrypted data and IV
+Write-Host "Encrypted Data (Base64): $encryptedBase64"
+Write-Host "Initialization Vector (Base64): $IVBase64"
+
+# # Decrypt the data
+# $AES = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+# $AES.Key = $keyBytes
+# $AES.Mode = [System.Security.Cryptography.CipherMode]::CBC
+
+# # Convert the IV and encrypted bytes from Base64
+# $IV = [System.Convert]::FromBase64String($IVBase64)
+# $encryptedBytes = [System.Convert]::FromBase64String($encryptedBase64)
+
+# # Create a decryptor
+# $decryptor = $AES.CreateDecryptor()
+# $decryptedBytes = $decryptor.TransformFinalBlock($encryptedBytes, 0, $encryptedBytes.Length)
+
+# # Convert the decrypted bytes back to plaintext
+# $decryptedText = [System.Text.Encoding]::UTF8.GetString($decryptedBytes)
+
+# # Display the decrypted data
+# Write-Host "Decrypted Data: $decryptedText"
